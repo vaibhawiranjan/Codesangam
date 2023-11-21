6d65af49468933dd13941977880b507f0db87c3f
Vaibhawi <vaibhawi.20223303@mnnit.ac.in>
2023-11-21 20:06:00 +0530

    Email feauture

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const User=require(`../models/userModel`)
 const jwt=require(`jsonwebtoken`)
-
+const nodemailer=require('nodemailer')
 const createToken=(_id)=>{
     return jwt.sign({_id},process.env.SECRET,{expiresIn:'1d'})
 }
@@ -12,6 +12,30 @@ const loginUser=async(req,res)=>{
      const user=await User.login(email,password)
      const token=createToken(user._id)
      res.status(200).json({email,token})
+     const sendMail=async(req,res)=>{
+          let transporter=await nodemailer.createTransport({
+              service:'gmail',
+              auth: {
+                // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+                user: 'annahartvr@gmail.com',
+                pass: 'wlfz qzge yveb hxyj'
+              }
+          });
+          const info = await transporter.sendMail({
+              from: '"VAIBHAWI 👻" <annahartvr@gmail.com>', // sender address
+              to: email, // list of receivers
+              subject: "Hello ✔", // Subject line
+              html: "<b>New login</b> <br> Complete pending tasks", // html body
+          });
+           try{
+     const result=await transporter.sendMail(info)
+     console.log("Email sent successfully")
+           }catch(error){
+          console.log(error)
+           }
+     }
+     
+     sendMail()
 }catch(error){
      res.status(404).json({error:error.message})
 }
