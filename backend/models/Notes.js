cef43742eba05ea3f5bc4f6fc7d1cf78bf036910
Saachi <saachi.20223219@mnnit.ac.in>
2023-11-21 19:35:57 +0530

    color feature

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -14,10 +14,10 @@ const noteSchema=new Schema({
         required:true
        
     },
-    user_id:{
-        type:String,
-        required:true
-    }
+     user_id:{
+         type:String,
+         required:true
+     }
 },{timestamps:true})
 
 module.exports=mongoose.model(`Notes`,noteSchema)
